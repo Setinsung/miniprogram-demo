986eb6725224fdc818166756e86fc33746ebe96e
Setinsung <1968989251@qq.com>
2023-10-16 23:04:49 +0800

    Convert to use uniapp

---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="box">
+		<h1>uniapp<span class="row">零基础</span>入门</h1>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+.box {
+	height: 100px;
+	background-color: pink;
+	h1 {
+		font-size: 40px;
+		color: red;
+		.row {
+			font-style: italic;
+		}
+	}
+}
+</style>
